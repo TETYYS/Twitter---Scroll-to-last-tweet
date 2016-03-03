c673eaf1ade5d31e0392d73e8523438fec09db97
tetyys <TETYYS@users.noreply.github.com>
2016-03-03 23:02:54 +0200

    Initial Release

---
+++ userscript.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Twitter - Scroll to last tweet
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Scrolls to latest tweet after pressing "View X new tweets" button
+// @author       TETYYS
+// @match        https://twitter.com/
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function(mutations, observer) {
+    var last = $('.stream ol li').get(0);
+    var bar = $('.js-new-tweets-bar');
+    if (bar.length != 0) {
+        var num = $('.js-new-tweets-bar').text().trim().match(/\d+/);
+        if (num > 5) {
+            bar.click(function() {
+                setTimeout(function() { last.scrollIntoView(); }, 500);
+            });
+        }
+    }
+});
+
+observer.observe(document, {
+  subtree: true,
+  attributes: true
+});
